f28a767486e68b5b83cd2b2464e9897f371c49bb
Marven <marvenwilsons@gmail.com>
2021-11-01 00:08:56 -0400

    added port easy access

--- config/app_starter/compose-builder.js
+++ config/app_starter/compose-builder.js
@@ -1,5 +1,7 @@
 const app_config = require('../app.json')
 
+const adminPort = 5000
+const publicPort = 3000
 /**************************************************************
  *                  Front End Admin Container                  *
  **************************************************************/
@@ -13,23 +15,23 @@ const frontEndAdminContainer = {
   working_dir: '/usr/src/admin',
   environment: [
     "APP_HOST=frontend-admin",
-    "APP_PORT=3000"
+    `APP_PORT=${adminPort}`
   ],
   ports: [
-    "3000:3000"
+    `${adminPort}:${adminPort}`
   ],
   volumes: [
-    "../frontend-admin/assets:/usr/src/nuxt-app/assets",
-    "../frontend-admin/components:/usr/src/nuxt-app/components",
-    "../frontend-admin/layouts:/usr/src/nuxt-app/layouts",
-    "../frontend-admin/middleware:/usr/src/nuxt-app/middleware",
-    "../frontend-admin/pages:/usr/src/nuxt-app/pages",
-    "../frontend-admin/plugins:/usr/src/nuxt-app/plugins",
-    "../frontend-admin/server:/usr/src/nuxt-app/server",
-    "../frontend-admin/store:/usr/src/nuxt-app/store",
-    "../frontend-admin/static:/usr/src/nuxt-app/static",
-    "../frontend-admin/m.js:/usr/src/nuxt-app/m.js",
-    "../frontend-admin/nuxt.config.js:/usr/src/nuxt-app/nuxt.config.js"
+    "../frontend-admin/assets:/usr/src/admin/assets",
+    "../frontend-admin/components:/usr/src/admin/components",
+    "../frontend-admin/layouts:/usr/src/admin/layouts",
+    "../frontend-admin/middleware:/usr/src/admin/middleware",
+    "../frontend-admin/pages:/usr/src/admin/pages",
+    "../frontend-admin/plugins:/usr/src/admin/plugins",
+    "../frontend-admin/server:/usr/src/admin/server",
+    "../frontend-admin/store:/usr/src/admin/store",
+    "../frontend-admin/static:/usr/src/admin/static",
+    "../frontend-admin/m.js:/usr/src/admin/m.js",
+    "../frontend-admin/nuxt.config.js:/usr/src/admin/nuxt.config.js"
   ]
 }
 /**************************************************************
@@ -42,27 +44,27 @@ const frontEndPublicContainer = {
   },
   container_name: 'Public',
   restart: 'unless-stopped',
-  working_dir: '/usr/src/nuxt-app',
+  working_dir: '/usr/src/public',
   environment: [
     'APP_HOST=frontend-public',
-    'APP_PORT=3001',
+    `APP_PORT=${publicPort}`,
     'APP_NAME=sample.com'
   ],
   ports: [
-    '3001:3001'
+    `${publicPort}:${publicPort}`
   ],
   volumes: [
-    "../frontend-public/assets:/usr/src/admin/assets",
-    "../frontend-public/components:/usr/src/admin/components",
-    "../frontend-public/layouts:/usr/src/admin/layouts",
-    "../frontend-public/middleware:/usr/src/admin/middleware",
-    "../frontend-public/pages:/usr/src/admin/pages",
-    "../frontend-public/plugins:/usr/src/admin/plugins",
-    "../frontend-public/server:/usr/src/admin/server",
-    "../frontend-public/store:/usr/src/admin/store",
-    "../frontend-public/static:/usr/src/admin/static",
-    "../frontend-public/m.js:/usr/src/admin/m.js",
-    "../frontend-public/nuxt.config.js:/usr/src/admin/nuxt.config.js"
+    "../frontend-public/assets:/usr/src/public/assets",
+    "../frontend-public/components:/usr/src/public/components",
+    "../frontend-public/layouts:/usr/src/public/layouts",
+    "../frontend-public/middleware:/usr/src/public/middleware",
+    "../frontend-public/pages:/usr/src/public/pages",
+    "../frontend-public/plugins:/usr/src/public/plugins",
+    "../frontend-public/server:/usr/src/public/server",
+    "../frontend-public/store:/usr/src/public/store",
+    "../frontend-public/static:/usr/src/public/static",
+    "../frontend-public/m.js:/usr/src/public/m.js",
+    "../frontend-public/nuxt.config.js:/usr/src/public/nuxt.config.js"
   ]
 }
 /**************************************************************
@@ -87,9 +89,9 @@ const nginxContainer = {
     "NGINX_HTTPS_PORT=8443",
     "NGINX_HOST=localhost",
     "NGINX_PORT=80",
-    "UPSTREAM_ADMIN=frontend-admin:3000",
+    `UPSTREAM_ADMIN=frontend-admin:${adminPort}`,
     `ADMIN_ROUTE=${app_config.admin_route}`,
-    "UPSTREAM_PUBLIC=frontend-public:3001",
+    `UPSTREAM_PUBLIC=frontend-public:${publicPort}`,
     "PGADMIN_URL=mydb"
   ]
 }
