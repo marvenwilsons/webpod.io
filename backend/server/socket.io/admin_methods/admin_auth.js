de16965f8be663ca330e25bfd91075599d436272
Marven <marvenwilsons@gmail.com>
2021-12-10 14:56:24 -0500

    added mock token authentication

--- backend/server/socket.io/admin_methods/admin_auth.js
+++ backend/server/socket.io/admin_methods/admin_auth.js
@@ -1,16 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken (token,secret,user) {
+  const payload = jwt.verify(token, secret)
+  return payload.user === user.id
+}
+
 module.exports = async function ({user, token}) {
-  const sampleUser = {
+  const u = {
     user: 'admin',
+    id: 'sampleUserIdz',
     password: 'admin',
     token: undefined
   }
   return new Promise(function(resolve,reject) {
+    // check user token, dashboard request authentication
     if(user) {
-      
-    
       setTimeout(() => {
         resolve({
-          is_valid: true
+          is_valid: verifyToken(token, process.env.JWT_SECRET,u)
         })
       }, 100)
     } else {
