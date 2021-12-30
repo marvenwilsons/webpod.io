9e1fb86111435bd774da49e20705191ddb6d8529
Marven <marvenwilsons@gmail.com>
2021-12-29 21:09:22 -0500

    added inputs

---
+++ frontend-admin/components/globals/form-builder/fields/calendar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import m from '../lib/m'
+export default {
+    mixins: [m],
+    props: ["data", "_key", "color", "key_index", "disabled"],
+    data: () => ({
+
+    }),
+    methods: {
+        
+    }
+}
+</script>
