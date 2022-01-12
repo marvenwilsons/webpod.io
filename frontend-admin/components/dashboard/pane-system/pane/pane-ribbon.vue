eb32613bf8be6b06bd242ededfcc2f28a6c5c08d
Marven <marvenwilsons@gmail.com>
2022-01-11 20:34:51 -0500

    refactor

--- frontend-admin/components/dashboard/pane-system/pane/pane-ribbon.vue
+++ frontend-admin/components/dashboard/pane-system/pane/pane-ribbon.vue
@@ -1,13 +1,16 @@
 <template>
   <main class=" flexstart" >
     <div class="flex" >
-      <span class="flex relative marginright050 flat_action" v-for="(ribbon, key) in ribbons" :key="uid(key)" >
-        <el-dropdown trigger="click" @command="ribbonItemClick" >
-          <span class="el-dropdown-link">
+      <span class="flex relative marginright050 flat_action " v-for="(ribbon, key) in ribbons" :key="uid(key)" >
+        <span style="color:#606266;" class="text-small2 flex flexcenter" v-if="ribbon.length < 1" >
+          {{key}}
+        </span>
+        <el-dropdown v-if="ribbon.length > 1" trigger="click" @command="ribbonItemClick" >
+          <span class="el-dropdown-link" style="font-size: 80%;">
             {{key}}
             <i  v-if="ribbon.length > 1"  class="el-icon-arrow-down el-icon--right"></i>
           </span>
-          <main>
+          <main  >
             <el-dropdown-menu   slot="dropdown">
               <el-dropdown-item :command="ribbon_item" v-for="ribbon_item in ribbon" :key="uid(ribbon_item)" >
                 {{ribbon_item}}
