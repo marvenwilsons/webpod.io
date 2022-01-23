17977ac34e51a6e738bb1a0867525821817b1b14
Marven <marvenwilsons@gmail.com>
2022-01-22 21:24:51 -0500

    added tile sizer

--- frontend-admin/components/dashboard/pane-system/tile/tile-s-size.vue
+++ frontend-admin/components/dashboard/pane-system/tile/tile-s-size.vue
@@ -3,22 +3,20 @@
         <span class="overline" > SIZE </span>
         <div class="flex" >
             <div class="flex" >
-                <div class="flex flexcol " >
+                <div class="flex flexcol flexcenter" >
                     <div class=" flex" >
-                        <div class="paneBorder padleft025" style="width:40px; max-height:18px" >
-                            0pt
+                        <div @click="width('minus')" style="min-height:9px;" class=" flex flexcenter" >
+                            <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M19,13H5V11H19V13Z" />
+                            </svg>
                         </div>
-                        <div>
-                            <div style="max-height:9px;" class=" flex flexcenter" >
-                                <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M7,15L12,10L17,15H7Z" />
-                                </svg>
-                            </div>
-                            <div style="max-height:9px;" class=" flex flexcenter" >
-                                <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M7,10L12,15L17,10H7Z" />
-                                </svg>
-                            </div>
+                        <div class="paneBorder padleft025" style="width:40px; max-height:18px" >
+                            {{widthValue}}pt
+                        </div>
+                        <div @click="width('add')" style="min-height:9px;" class=" flex flexcenter" >
+                            <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+                            </svg>
                         </div>
                     </div>
                     <div class="marginleft025" >
@@ -29,22 +27,21 @@
                 </div>
             </div>
             <div class="flex" >
-                <div class="flex flexcol " >
+                <div class="flex flexcol flexcenter" >
                     <div class=" flex" >
-                        <div class="paneBorder padleft025" style="width:40px; max-height:18px" >
-                            0pt
+                        
+                        <div @click="height('minus')" style="min-height:9px;" class=" flex flexcenter" >
+                            <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M19,13H5V11H19V13Z" />
+                            </svg>
                         </div>
-                        <div>
-                            <div style="max-height:9px;" class=" flex flexcenter" >
-                                <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M7,15L12,10L17,15H7Z" />
-                                </svg>
-                            </div>
-                            <div style="max-height:9px;" class=" flex flexcenter" >
-                                <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
-                                    <path fill="currentColor" d="M7,10L12,15L17,10H7Z" />
-                                </svg>
-                            </div>
+                        <div class="paneBorder padleft025" style="width:40px; max-height:18px" >
+                            {{heightValue}}pt
+                        </div>
+                        <div @click="height('add')" style="min-height:9px;" class=" flex flexcenter" >
+                            <svg class="pointer" style="width:20px;height:20px" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+                            </svg>
                         </div>
                     </div>
                     <div class="marginleft025" >
@@ -57,3 +54,27 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        widthValue: 0,
+        heightValue: 0
+    }),
+    methods: {
+        width(condition) {
+            this.$parent.width(condition,null,this.$parent.nodeSelectedIndex)
+            this.widthValue = this.$parent.tiles[this.$parent.nodeSelectedIndex].colEnd
+        },
+        height(condition) {
+            this.$parent.height(condition,null,this.$parent.nodeSelectedIndex)
+            this.heightValue = this.$parent.tiles[this.$parent.nodeSelectedIndex].rowEnd
+        }
+    },
+    mounted() {
+        this.heightValue = this.$parent.tiles[this.$parent.nodeSelectedIndex].rowEnd
+        this.widthValue = this.$parent.tiles[this.$parent.nodeSelectedIndex].colEnd
+
+    }
+}
+</script>
