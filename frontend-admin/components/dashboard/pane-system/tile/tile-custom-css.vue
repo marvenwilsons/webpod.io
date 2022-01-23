e0314169fe51b4cc5a9a6e275ab47092f368b6a2
Marven <marvenwilsons@gmail.com>
2022-01-22 21:41:12 -0500

    added tile custom css

---
+++ frontend-admin/components/dashboard/pane-system/tile/tile-custom-css.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flexcol" >
+        <span class="overline" > TILE CUSTOM STYLE </span>
+        <span class="margintop050 marginbottom125" >
+            Style to be applied to this selected tile only.
+        </span>
+        <div>
+            <!-- <textarea name="" id="" cols="30" rows="10"></textarea> -->
+            <codeEditor
+                :readOnly="false"
+                :lang="'css'"
+            ></codeEditor>
+        </div>
+    </div>
+</template>
