da53bfe464dc0ec016aa905c014cbe6c08b3c66c
Marven <marvenwilsons@gmail.com>
2022-01-25 11:09:10 -0500

    added z-index

--- frontend-admin/components/dashboard/pane-system/tile/tile-s-z-index.vue
+++ frontend-admin/components/dashboard/pane-system/tile/tile-s-z-index.vue
@@ -1,23 +1,42 @@
 <template>
     <div>
-        <span class="overline" > Z-INDEX </span>
-        <div @click="moveToFront" class="paneBorder  flex flexcenter flat_action" >
-            <div>Move to front</div>
+        <span class="overline" > ARANGE </span>
+        <div @click="moveToFront" class="paneBorder  flex flexcenter " >
+            <v-btn tile plain block >Move to front  </v-btn>
         </div>
-        <div @click="moveToBack" class="paneBorder margintop025  flex flexcenter flat_action" >
-            <div>Move to Back</div>
+        <div @click="moveToBack" class="paneBorder margintop025  flex flexcenter " >
+            <v-btn tile plain block >Move to back</v-btn>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data: () => ({
+        count: 0
+    }),
     methods: {
         moveToFront() {
-            console.log('move to front')
+            this.count = this.count + 1
+            
+            const parent = this.$parent
+            parent.tiles[parent.nodeSelectedIndex].customStyle.zIndex = this.count
+            parent.refresh()
         },
         moveToBack() {
-            console.log('move to back')
+            this.count = this.count - 1
+            const parent = this.$parent
+            parent.tiles[parent.nodeSelectedIndex].customStyle.zIndex = this.count
+            parent.refresh()
+        }
+    },
+    mounted() {
+        const parent = this.$parent
+        const zIndex = parent.tiles[parent.nodeSelectedIndex].customStyle.zIndex
+        if(zIndex != undefined) {
+            this.count = parent.tiles[parent.nodeSelectedIndex].customStyle.zIndex
+        } else {
+            // parent.tiles[parent.nodeSelectedIndex].customStyle.zIndex = 1
         }
     }
 }
