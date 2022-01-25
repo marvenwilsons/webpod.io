d3f8db09489634edcadc41ac85dd59bc0fd4b45e
Marven <marvenwilsons@gmail.com>
2022-01-24 21:18:55 -0500

    remove view actions. should in separate pane

--- frontend-admin/components/dashboard/pane-system/tile/tiles-s-view.vue
+++ frontend-admin/components/dashboard/pane-system/tile/tiles-s-view.vue
@@ -4,10 +4,12 @@
         <div  class="caption marginbottom125">
             Tile view is an app instance, select an app and its instance to be displayed on the tile
         </div>
-        <div class="paneBorder flat_action flex flexcenter" >
-            Select Tile View
+        <div class="paneBorder  flex flexcenter" >
+            <v-btn plain block flat_action tile class="flat_action" >
+                Select Tile View
+            </v-btn>
         </div>
-        <div style="border-left: 1px solid #efefef;" class="marginleft050">
+        <!-- <div style="border-left: 1px solid #efefef;" class="marginleft050">
             <div class="flat_action flex spacebetween" >
                 View Data <code> <i>{} | []</i> </code>
             </div>
@@ -23,6 +25,6 @@
             <div class="flat_action flex spacebetween" >
                 View Interface
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
