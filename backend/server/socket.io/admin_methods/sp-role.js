253ccb73aee8207bbd588edfe11b83af53bf4fa5
Marven <marvenwilsons@gmail.com>
2022-01-29 10:37:56 -0500

    mocked role

--- backend/server/socket.io/admin_methods/sp-role.js
+++ backend/server/socket.io/admin_methods/sp-role.js
@@ -2,8 +2,66 @@ async function insertNewRole () {
 
 }
 
-async function getRole () {
+async function getRole (role_id) {
+  console.log(`=== getRole ${role_id} ===`)
 
+  const mock_response = {
+    role_id,
+    role_name: 'master',
+    role_menu: [
+      {
+        menu_id: 'menu-id-lkasdn',
+        service_id: 'service-id-dmni24',
+        service_version: 'sevice-ver-nvjb23y8'
+      },
+      {
+        menu_id: 'menu-id-fhtte45y',
+        service_id: 'service-id-sdgasdf',
+        service_version: 'sevice-ver-wfmiuwfh'
+      },
+      {
+        menu_id: 'menu-id-fhytj34r',
+        service_id: 'service-id-prch',
+        service_version: 'sevice-ver-dffr4yt'
+      },
+      {
+        menu_id: 'menu-id-fesseft',
+        service_id: 'service-id-h6tgwer23',
+        service_version: 'sevice-ver-nvjbf23y8'
+      },
+      {
+        menu_id: 'menu-id-hfg45uc',
+        service_id: 'service-id-dmni25',
+        service_version: 'sevice-ver-nvjb23y8'
+      },
+      {
+        menu_id: 'menu-id-lkasdn22',
+        service_id: 'service-id-dmni26',
+        service_version: 'sevice-ver-nvjb23y11'
+      },
+      {
+        menu_id: 'menu-id-lkasdn1',
+        service_id: 'service-id-dmni25',
+        service_version: 'sevice-ver-nvjb23y9'
+      },
+      {
+        menu_id: 'menu-id-lkasdn28',
+        service_id: 'service-id-dmni27',
+        service_version: 'sevice-ver-nvjb23y10'
+      },
+      {
+        menu_id: 'menu-id-lkasdn',
+        service_id: 'service-id-dmni24',
+        service_version: 'sevice-ver-nvjb23y21'
+      }
+    ]
+  }
+
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(mock_response)
+    }, 50)
+  })
 }
 
 async function getAllRoles () {
