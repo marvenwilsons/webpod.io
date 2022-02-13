873c9e66dcdf3a2d008f5ad54fd21d94b527a1bd
Marven <marvenwilsons@gmail.com>
2022-02-12 22:15:20 -0500

    fixed account button overflowing on small width

--- frontend-admin/layouts/default.vue
+++ frontend-admin/layouts/default.vue
@@ -51,19 +51,8 @@
                 </v-fade-transition>
             </main>
             <main v-show="showDashboard" class="flex fullheight-percent " style="background:#7fccff;" >
-
                 <!-- BACKGROUND -->
-                <div class="fullwidth" style="position:fixed;z-index:1" >
-                    <svg 
-                        viewBox="0 0 1440 320"
-                        >
-                        <path 
-                            fill="#0099ff" 
-                            fill-opacity="1"
-                            d="M0,320L48,304C96,288,192,256,288,229.3C384,203,480,181,576,186.7C672,192,768,224,864,208C960,192,1056,128,1152,138.7C1248,149,1344,235,1392,277.3L1440,320L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
-                            </path>
-                    </svg>
-                </div>
+                <accent-bg/>
                 <!-- menubar -->
                 <section style="z-index:2" class="pane" >
                     <v-card dark style="z-index:800; min-width:220px;" class="flexcol margin050" >
@@ -72,17 +61,10 @@
                 </section>
                 <!-- CONTENT -->
                 <section class="fullwidth flex pane" style="z-index:2" >
-                    <div style="background: #232729; color: #009aff; display: none;"  class="pad025" >
-                        <topbar :notificationLength="notifications.length" :user="user" @openNotificationWindow="sidebarWindowIsOpen = !sidebarWindowIsOpen" ref="topbar" />
-                        <div>
-                            <history @historyClick="historyClick" ref="history" />
-                        </div>
-                    </div>
                     <section class="flex fullwidth" >
                         <nuxt ref="pane" />
                     </section>
-                    <div style="z-index: 1900; right: 0; bottom: 0;" 
-                    class="absolute  flex flexend" >
+                    <div  style="z-index: 1900; right: 0; bottom: 0; max-width: 5%;" class="absolute flex flexend" >
                         <div class="marginright125 marginbottom125 flex flexcol" >
                             <v-scale-transition origin="center">
                                 <v-btn
@@ -133,15 +115,14 @@
 import menubar from '@/components/dashboard/menu-bar/index' 
 import service from '@/components/dashboard/services/index'
 import dashboard from '@/components/dashboard/dashboard.js'
-import topbar from '@/components/dashboard/topbar/index.vue'
-import history from '@/components/dashboard/topbar/history.vue'
 import sidebar from '@/components/dashboard/side-bar/index.vue'
 import notification from '@/components/dashboard/side-bar/notification.vue'
+import accentBg from '@/components/dashboard/accent-bg/index.vue'
 
 import m from '@/m'
 export default {
     mixins: [m],
-    components: {menubar, topbar, sidebar, history, notification},
+    components: {menubar, sidebar, notification, accentBg},
     data: () => ({
         panes: [],
         showModal: false,
@@ -245,10 +226,6 @@ export default {
              // component references
             const menu = this.$refs.menubar
             const paneCollection = this.$refs.pane.$children[0].$children[0]
-            const topbar =  {
-                ...this.$refs.topbar,
-                history: this.$refs.history
-            }
             const sidebar = {
                 open: () => this.sidebarWindowIsOpen = true,
                 close: () => this.sidebarWindowIsOpen = false,
