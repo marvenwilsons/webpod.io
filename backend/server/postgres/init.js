ebbc8a2044d0b4cddb1d4162cb1c86467e995490
Marven <marvenwilsons@gmail.com>
2022-02-20 06:22:08 -0500

    added init protocol

--- backend/server/postgres/init.js
+++ backend/server/postgres/init.js
@@ -7,7 +7,7 @@ async function GET_ALL_TABLES () {
     return res.rows.length == 0 ? 'no tables found' : res.rows.length
 }
 
-module.exports = async (body) => {
+module.exports = async (body, cb) => {
     const {firstName, lastName, applicationName, username, password, email, databaseName, databaseUsername, tablePrefix, databasePassword} = body
     console.log(body)
     /**
@@ -25,7 +25,7 @@ module.exports = async (body) => {
         try {
             global.log('installing uuid-ossp extension')
             global.progress('25%')
-            uuid_ossp_ext_res = (await query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`)).rows.length == 0 && 'success'
+            uuid_ossp_ext_res = (await query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`)).rows.length == 0 && 'OK'
         } catch(err) {
             console.log('ossp_error', err)
         }
@@ -38,11 +38,11 @@ module.exports = async (body) => {
      * Create postgres database user
      */
     let insert_user_result = undefined
-    if(uuid_ossp_ext_res == 'success') {
+    if(uuid_ossp_ext_res == 'OK') {
         try {
             global.log(`inserting database user: ${databaseUsername} ...`)
             global.progress('30%')
-            insert_user_result = (await query(`CREATE USER ${databaseUsername}`)).rows.length == 0 && 'success'
+            insert_user_result = (await query(`CREATE USER ${databaseUsername}`)).rows.length == 0 && 'OK'
         } catch(err) {
             console.log('create user error', err)
         }
@@ -52,11 +52,11 @@ module.exports = async (body) => {
      * Granting all orivileges on database
      */
     let user_privilege_grant = undefined
-    if(insert_user_result === 'success' ) {
+    if(insert_user_result === 'OK' ) {
         try {
             global.log(`granting database privilages to ${databaseUsername}`)
             global.progress('55%')
-            user_privilege_grant = (await query(`GRANT ALL PRIVILEGES ON DATABASE ${databaseName} TO ${databaseUsername}`)).rows.length == 0 && 'success'
+            user_privilege_grant = (await query(`GRANT ALL PRIVILEGES ON DATABASE ${databaseName} TO ${databaseUsername}`)).rows.length == 0 && 'OK'
         } catch(err) {
             console.log('grant error', err)
         }
@@ -67,11 +67,11 @@ module.exports = async (body) => {
      * Setting database user password
      */
     let set_db_password_result = undefined
-    if(user_privilege_grant === 'success') {
+    if(user_privilege_grant === 'OK') {
        try {
             global.log(`setting database password for ${databaseUsername} ...`)
             global.progress('60%')
-            set_db_password_result = (await query(`ALTER USER ${databaseUsername} PASSWORD '${databasePassword}'`)).rows.length == 0 && 'success'
+            set_db_password_result = (await query(`ALTER USER ${databaseUsername} PASSWORD '${databasePassword}'`)).rows.length == 0 && 'OK'
        } catch(err) {
            console.log('set user password err', err)
        }
@@ -82,7 +82,7 @@ module.exports = async (body) => {
      * database user and webpod user is different
      */
     let create_user_table_result = undefined
-    if(set_db_password_result === 'success') {
+    if(set_db_password_result === 'OK') {
         global.log('Creating users table ...')
         global.progress('70%')
         try {
@@ -96,7 +96,7 @@ module.exports = async (body) => {
                     last_name VARCHAR(100) NOT NULL,
                     role_id uuid [ ]
                 )`
-            )).rows.length == 0 && 'success'
+            )).rows.length == 0 && 'OK'
         } catch(err) {
             console.log('creating users table error ',err)
         }
@@ -106,7 +106,7 @@ module.exports = async (body) => {
      * Create roles table
      */
     let create_roles_table_result = undefined
-    if(create_user_table_result === 'success') {
+    if(create_user_table_result === 'OK') {
         global.log('Creating roles table ...')
         global.progress('75%')
         
@@ -116,14 +116,14 @@ module.exports = async (body) => {
                 role_name VARCHAR(500) NOT NULL, 
                 role_menu uuid [ ]
             )`
-        )).rows.length == 0 && 'success'
+        )).rows.length == 0 && 'OK'
     }
 
     /**
      * Add default master role
      */
     let master_role_id = undefined
-    if(create_roles_table_result === 'success') {
+    if(create_roles_table_result === 'OK') {
         global.log('Inserting master role ...')
         global.progress('80%')
         try {
@@ -149,7 +149,7 @@ module.exports = async (body) => {
                     menu_id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
                     menu_name VARCHAR(500) NOT NULL
                 )`
-            )).rows.length == 0 && 'success'
+            )).rows.length == 0 && 'OK'
         } catch(err) {
             console.log('create menu table err ', err)
         }
@@ -160,7 +160,7 @@ module.exports = async (body) => {
      */
     let add_default_menu_result = undefined
     let default_menu_uids = []
-    if(create_menu_table_result === 'success') {
+    if(create_menu_table_result === 'OK') {
         global.log('Inserting default menus ...')
         try {
             const default_menus = ['Dashboard','Site','Collections','Apps','Users','Roles','Services','Settings']
@@ -173,7 +173,7 @@ module.exports = async (body) => {
             
             add_default_menu_result = await Promise.all(qs).then(data => {
                 default_menu_uids = data.map(r => r.rows[0].menu_id)
-                return 'success'
+                return 'OK'
             })
         } catch(err) {
             console.log('insert default menus err ', err)
@@ -185,21 +185,21 @@ module.exports = async (body) => {
      * Update master role add menus
      */
     let update_role_menu_result = undefined
-    if(add_default_menu_result === 'success') {
+    if(add_default_menu_result === 'OK') {
         global.log('Updating role menus ...')
 
         update_role_menu_result = (await query(`
             UPDATE ${tablePrefix}roles 
             SET role_menu = '{${default_menu_uids.join(',')}}'
             WHERE role_id = '${master_role_id}'
-        `)).rows.length == 0 && 'success'
+        `)).rows.length == 0 && 'OK'
     }
     
     /**
      * Create a master dashboard admin user
      */
     let create_admin_user_result = undefined
-    if(update_role_menu_result === 'success') {
+    if(update_role_menu_result === 'OK') {
         try {
             const saltRound = 10
             const salt =  bcryptjs.genSaltSync(saltRound)
@@ -207,7 +207,7 @@ module.exports = async (body) => {
             create_admin_user_result = (await query(`
                 INSERT INTO ${tablePrefix}users (email,username,user_password,first_name,last_name,role_id)
                 VALUES ($1, $2, $3, $4, $5, $6)
-            `,[email, username, bcryptPassword, firstName, lastName, `{${master_role_id}}`])).rows.length == 0 && 'success'
+            `,[email, username, bcryptPassword, firstName, lastName, `{${master_role_id}}`])).rows.length == 0 && 'OK'
         } catch(err) {
             console.log('insert user err', err)
         }
@@ -217,7 +217,7 @@ module.exports = async (body) => {
      * Create apps table
      */
     let create_apps_table_result = undefined
-    if(create_admin_user_result == 'success') {
+    if(create_admin_user_result == 'OK') {
         try {
             global.log('Creating apps table ...')
 
@@ -240,7 +240,7 @@ module.exports = async (body) => {
                         modified_by uuid REFERENCES ${tablePrefix}users(user_id)
                     )
                 `)
-            ]).then(data => 'success')
+            ]).then(data => 'OK')
         } catch(err) {
             console.log('create_apps_table_err', err)
         }
@@ -251,7 +251,7 @@ module.exports = async (body) => {
      * Create service table
      */
     let create_services_table_result = undefined
-    if(create_apps_table_result == 'success') {
+    if(create_apps_table_result == 'OK') {
         global.log('Creating services table ...')
         global.progress('90%')
 
@@ -278,5 +278,9 @@ module.exports = async (body) => {
         global.log('done')
         global.progress('100%')
         console.log('its done')
+        
+        if(cb) {
+            cb('OK')
+        }
     }
 }
