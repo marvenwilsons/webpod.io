ea276b9d89b1f00b17428a8c6b7864b6ce5d43b0
Marven <marvenwilsons@gmail.com>
2022-03-01 16:07:57 -0500

    added resizable component

---
+++ frontend-admin/components/globals/resizable/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="background: red; " id="resizable" class=" borderred pad125 fullwidth" >
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        
+
+    }
+}
+</script>
