b12a76c153454d5d88231b7cf1dc46cd9a06bc27
Marven <marvenwilsons@gmail.com>
2022-03-01 15:31:25 -0500

    added header

--- frontend-admin/components/globals/draggable/index.vue
+++ frontend-admin/components/globals/draggable/index.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="fullwidth flex flexcenter" >
-        <div  id="draggable" class="absolute draggable" >
-            <div v-if="ready" class="absolute flex flexend fullwidth" style="z-index: 1;" :id="`draggableheader`" >
-                <v-btn :ripple="false" style="cursor: move;" text plain small tile >
-                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M3,15V13H5V15H3M3,11V9H5V11H3M7,15V13H9V15H7M7,11V9H9V11H7M11,15V13H13V15H11M11,11V9H13V11H11M15,15V13H17V15H15M15,11V9H17V11H15M19,15V13H21V15H19M19,11V9H21V11H19Z" />
-                    </svg>
-                </v-btn>
+        <v-card id="mydiv">
+            <div id="mydivheader" class="pad025 flex flexend">
+                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M3,15V13H5V15H3M3,11V9H5V11H3M7,15V13H9V15H7M7,11V9H9V11H7M11,15V13H13V15H11M11,11V9H13V11H11M15,15V13H17V15H15M15,11V9H17V11H15M19,15V13H21V15H19M19,11V9H21V11H19Z" />
+                </svg>
             </div>
-            <div  role="content" >
+            <div style="z-index:2" role="content" >
                 <slot></slot>
             </div>
-        </div>
+        </v-card>
     </div>
 </template>
 
@@ -25,63 +23,63 @@ export default {
         ready: false
     }),
     mounted() {
-        this.currentId = this.uid()
-        const position = { x: 0, y: 0 }
-        // this.$interact('.draggable').draggable({
-        //     listeners: {
-        //         move (event) {
-        //             position.x += event.dx
-        //             position.y += event.dy
-
-        //             event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
-        //         },
-        //     }
-        // })
-
-        // Make the DIV element draggable:
+        dragElement(document.getElementById("mydiv"));
 
         function dragElement(elmnt) {
-            var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-            if (document.getElementById(elmnt.id + "header")) {
-                // if present, the header is where you move the DIV from:
-                document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-            } else {
-                // otherwise, move the DIV from anywhere inside the DIV:
-                elmnt.onmousedown = dragMouseDown;
-            }
-
-            function dragMouseDown(e) {
-                e = e || window.event;
-                e.preventDefault();
-                // get the mouse cursor position at startup:
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                document.onmouseup = closeDragElement;
-                // call a function whenever the cursor moves:
-                document.onmousemove = elementDrag;
-            }
-
-            function elementDrag(e) {
-                e = e || window.event;
-                e.preventDefault();
-                // calculate the new cursor position:
-                pos1 = pos3 - e.clientX;
-                pos2 = pos4 - e.clientY;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                // set the element's new position:
-                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-            }
-
-            function closeDragElement() {
-                // stop moving when mouse button is released:
-                document.onmouseup = null;
-                document.onmousemove = null;
-            }
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementById(elmnt.id + "header")) {
+            // if present, the header is where you move the DIV from:
+            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        } else {
+            // otherwise, move the DIV from anywhere inside the DIV:
+            elmnt.onmousedown = dragMouseDown;
         }
-        dragElement(document.getElementById("draggable"));
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+        }
 
     },
 }
 </script>
+
+<style>
+#mydiv {
+  position: absolute;
+  z-index: 9;
+}
+
+#mydivheader {
+  cursor: move;
+  z-index: 10 !important;
+  background-color: #1976D2;
+  color: #fff;
+}
+</style>
