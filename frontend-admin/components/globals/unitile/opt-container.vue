8b7819bd3ec83ae8191150c05e54d49eb442ec32
Marven <marvenwilsons@gmail.com>
2022-03-08 18:08:51 -0500

    added ribbon option container

---
+++ frontend-admin/components/globals/unitile/opt-container.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-sheet style="height:40px;" elevation="0" class="pad050 borderRad4" >
+        <div class="flex flexcenter fullheight-percent" >
+            <div class="flex flexcenter  marginright050  text-caption" >
+                {{title}}
+            </div>
+            <div class="flex flexcenter" >
+                <slot></slot>
+            </div>
+        </div>
+    </v-sheet>
+</template>
+<script>
+export default {
+    props: ['title']
+}
+</script>
