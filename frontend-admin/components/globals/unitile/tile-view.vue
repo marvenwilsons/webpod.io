2a16805f1e528370467ed94934ac32ce39b8384d
Marven <marvenwilsons@gmail.com>
2022-03-28 21:53:07 -0400

    tile view

--- frontend-admin/components/globals/unitile/tile-view.vue
+++ frontend-admin/components/globals/unitile/tile-view.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-        {{tile}}
+    <div class="flex relative fullheight-percent" >
+        <div 
+        :style="{order: layer.layer_order}" 
+        class="absolute fullwidth fullheight-percent" 
+        v-for="layer in layers" :key="uid(layer)" 
+        >
+            <layer
+            :backgroundColor="layer.background"
+            :opacity="layer.opacity"
+            :show="layer.show"
+            >
+                layer: {{layer}}
+            </layer>
+        </div>
     </div>
 </template>
 
 <script>
+import m from '@/m'
+import layer from './layer.vue'
 export default {
-    props: ['tile']
+    mixins: [m],
+    props: ['tile'],
+    components: {layer},
+    data: () => ({
+        layers: []
+    }),
+    created() {
+        this.layers = this.tile.layers
+    }
 }
 </script>
