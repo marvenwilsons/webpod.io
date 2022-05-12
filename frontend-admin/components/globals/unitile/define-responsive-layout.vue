f58cef6dde0b53ff3bc7227d0149823ed037e334
Marven <marvenwilsons@gmail.com>
2022-05-12 08:57:47 -0400

    refactor

--- frontend-admin/components/globals/unitile/define-responsive-layout.vue
+++ frontend-admin/components/globals/unitile/define-responsive-layout.vue
@@ -1,5 +1,8 @@
 <template>
-    <div style="width:600px;" >
+    <div style="width:400px;" >
+        <div>
+            Select width variant for this layout
+        </div>
         <!-- add -->
         <div class="flex flexend" >
             <v-btn @click="addLayoutEntry" tile icon >
@@ -7,13 +10,37 @@
             </v-btn>
         </div>
         <!-- representation -->
-        <div style="min-height: 40px; flex-direction: row-reverse; gap: 3px;" class="flex paneBorder flexend pad025" >
+        <div style="min-height: 50px; gap: 3px;" 
+        class="flex paneBorder flexcol pad025" >
             <v-card v-ripple v-for="(layout,index) in layouts" :key="index"
-            :class="['flex flexcenter pointer light-blue darken-4',layout.name == 'default' ? 'fullwidth' : '']"
-            :style="{width: layout.name != 'default' ? `${layout.startLayoutOn}px`  : '100%', minWidth: '50px'}"
+            :class="['flex flexcenter pointer light-blue darken-4 pad125',layout.name == 'default' ? 'fullwidth' : '']"
+            :style="{ minWidth: '50px'}"
             >
-                <div style="font-family: 'Menlo'" class="flex fullheight-percent flexcenter white--text" >
-                    {{layout.name != 'default' ? `${layout.startLayoutOn}px` : 'wide - *'}}
+                <div style="font-family: 'Menlo'" class="flex fullheight-percent flexcol white--text" >
+                    <div class="flex spacebetween marginbottom025" >
+                        <strong>{{layout.name != 'default' ? `${layout.startLayoutOn}px` : 'Default'}}</strong>
+                        <dropDown
+                            v-if="layout.name != 'default'"
+                            @command="(title) => handleCmd(title,layout.startLayoutOn)"
+                            :options="[
+                                {title: 'Remove', d: 'M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z'},
+                                {title: 'Update trigger'},
+                                {title: 'Copy trigger layout content'}
+                            ]"
+                        >
+                            <v-btn small icon >
+                                <v-icon color="white" >mdi-dots-vertical</v-icon>
+                            </v-btn>
+                        </dropDown>
+                        
+                    </div>
+                    <div v-if="layout.name == 'default'" class="caption" >
+                        This layout trigger will be activated starting on <strong v-if="layouts[1]" >{{layouts[1].startLayoutOn}}px</strong> up until screen fullwidth.
+                    </div>
+                    <div class="caption" v-if="layout.name != 'default' " >
+                        This layout trigger will be actiavated starting on <strong>{{layouts[index].startLayoutOn}}px</strong> up until to
+                        <strong>{{layouts[index + 1] == undefined ? 'lowest width' : layouts[index + 1].startLayoutOn + 'px'}}</strong>
+                    </div>
                 </div>
             </v-card>
         </div>
@@ -28,12 +55,11 @@ export default {
         layouts: [
             {
                 name: 'default',
-                layout: null
-
+                layout: null,
             }, 
             {
                 name: 'medium',
-                startLayoutOn: 600 // activating layout starting 600px below
+                startLayoutOn: 300 // activating layout starting 600px below
             },
         ],
         errorMsg: null
@@ -49,26 +75,51 @@ export default {
                     startLayoutOn: selectedWidth,
                     layout: null
                 })
+                webpod.dash.modal.setData(this.layouts)
             }
             
             if(this.layouts.length == 1) {
                 addEntry()
-                webpod.dash.modal.setData(this.layouts)
             } else {
                 const lastLayout = this.layouts[this.layouts.length - 1]
                 if(prsElement.textContent != 'N/A') {
-                    if(lastLayout.startLayoutOn == selectedWidth || lastLayout.startLayoutOn < selectedWidth) {
+                    if(lastLayout.startLayoutOn == selectedWidth) {
                         webpod.dash.bottomAlert(`new entry should have lower width than ${selectedWidth}px`)
                     } else {
                         addEntry()
+                        this.layouts = this.layouts.sort((a,b) => b.startLayoutOn - a.startLayoutOn)
                     }
                 } else {
-                    webpod.dash.bottomAlert(`new entry should have lower width than 100%`)
+                    webpod.dash.bottomAlert(`new entry should have lower width than current fullwidth value`)
                 }
                 
             }
 
             
+        },
+        getUnclaimedSpace() {
+
+        },
+        updateWidth() {
+            const selectedWidth = document.getElementById('prs').textContent
+            if(selectedWidth != 'N/A') {
+                const v = parseInt(selectedWidth)
+            }
+        },
+        handleCmd(commandTitle,startLayoutOn) {
+            if(commandTitle == 'Update trigger') {
+                this.updateWidth()
+            }
+
+            if(commandTitle == 'Remove') {
+                for(let i = 0; i < this.layouts.length; i++) {
+                    if(this.layouts[i].startLayoutOn == startLayoutOn) {
+                        this.layouts.splice(i,1)
+                        break
+                    }
+                }
+                // this.layouts.splice(1)
+            }
         }
     }
 }
--- frontend-admin/components/globals/unitile/mixins/responsive-options.js
+++ frontend-admin/components/globals/unitile/mixins/responsive-options.js
@@ -2,8 +2,13 @@ export default {
     methods:{
         showResponsiveOptionModal() {
             const modalInstance = webpod.dash.modal.show({
-                modalTitle: 'Define responsive layout',
+                modalTitle: 'Select layout variant',
                 viewTrigger: (v) =>  this.$set(this.modals,'define_responsive_layout',v ? 'show' : 'hide')
+            })
+
+            modalInstance.on('data', (data) => {
+                console.log(data)
+                // data = {firstName: 'Jane', lastName: 'Doe}
             })
         }
     }
