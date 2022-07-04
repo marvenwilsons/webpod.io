b2c4509444c7eba76bc566233d3c76688864437b
Marven <marvenwilsons@gmail.com>
2022-07-04 10:29:19 -0400

    refactor

--- frontend-admin/components/globals/wp-role/users/home-widget.vue
+++ frontend-admin/components/globals/wp-role/users/home-widget.vue
@@ -1,11 +1,14 @@
 <template>
-    <v-card elevation="3" class="rounded-lg" >
+    <v-card elevation="0" outlined class="rounded-lg" >
+        <!-- <div>
+            <v-icon color="#6a489a" style="transform: rotate(90deg);" >mdi-triangle</v-icon>
+        </div> -->
         <div style="min-width: 120px; min-height: 100px;" 
         class="body-1 pad125 flex flexcenter flexcol " >
             <h5 style="margin:0; font-weight: 300;" >
                 {{data}}
             </h5>
-            <div class="caption margintop025 grey--text darken-1" >
+            <div class="caption margintop050 grey--text darken-1 text-uppercase" >
                 {{title}}
             </div>
         </div>
