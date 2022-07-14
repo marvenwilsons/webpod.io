0aecb038af0f1a2a24d30543cbce15a14d09fe30
marvenwilsons <marvenwilsons@gmail.com>
2022-07-13 21:45:45 -0400

    refactor wp_get server

--- backend/server/express/wp-get.js
+++ backend/server/express/wp-get.js
@@ -11,36 +11,37 @@ function isValidDomain(domainName) {
     return domains.includes(domainName) ? true : false
 }
 
+function transformValues(domain,rows) {
+    if(domain == 'users') {
+        // decrypt password
+    }
+    return rows
+}
+
 function fetchAllDataFromDomain(db,domain) {
     return new Promise((resolve,reject) => {
         if(isValidDomain(domain)) {
-            if(domain == 'users') {
-                console.log('> Fetching all users using wp_get')
-                db.emit('list-all-users', (rows) => {
-                    resolve({key: 'users', value: rows})
-                })
-            }
-    
-            if(domain == 'collections') {
-                resolve({collections: 'hello'})
+            console.log(`> Fetching all ${domain} using wp_get`)
+            try {
+                db.emit(`list-all-${domain}`, (rows) => 
+                resolve({key: domain, value: transformValues(domain,rows)}))
+            } catch(err) {
+                reject(err)
             }
         }
     })
 }
 
 function fetchAllDataFromDomainWithFilter(db,domain,filter) {
-    console.log(2)
     return new Promise((resolve,reject) => {
         resolve({key: domain, value: 'hey'})
     })
 }
 
 module.exports= (app,db) => {
-    app.post('/wp_get', (req,res) => {
-        const routesArray = req.body
-
-        if(Array.isArray(routesArray)) {
-            const x = routesArray.map(async route => 
+    app.post('/wp_get', ({body},res) => {
+        if(Array.isArray(body)) {
+            const x = body.map(async route => 
                 (getMode(route) == 'domain only' && await fetchAllDataFromDomain(db, route.domain) ||
                 getMode(route) == 'domain with filter' && await fetchAllDataFromDomainWithFilter(db,route.domain,route.filters))
             )
@@ -51,15 +52,5 @@ module.exports= (app,db) => {
         } else {
             console.log('wp_get should a type of array')
         }
-
-        /**
-         * sample input:
-         * [{domain: 'users', domain: 'collections'}]
-         * sample output:
-         * {
-         *  users: [...],
-         *  collections: [...]
-         * }
-         */
     })
 }
