475eb9d990ba7efaf0aaaa9f22d5351d1f4020c6
marvenwilsons <marvenwilsons@gmail.com>
2022-07-19 08:24:01 -0400

    create a new user route

--- backend/server/express/users.js
+++ backend/server/express/users.js
@@ -3,7 +3,13 @@ const db = require('../postgres/events')
 module.exports = (app) => {
     app.get('/users', async (req,res) => db.emit('list-all-users', users => res.json(users)) )
 
+    // fetch a specific user
     app.get('/user/:userId', async (req,res) => {
         
+    })
+
+    // add a user
+    app.post('/user', async (req, res) => {
+        // add a new user
     })
 }
